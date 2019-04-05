ce409758e528b38df6d3737146520023a627ba9f
Corten Singer <cortensinger@Cortens-MacBook-Pro.local>
2019-04-05 15:39:48 -0700

    Initial commit of iOS/Android app skeletons. UWP coming later. Xamarin Forms bug on MainPage, where Buttons dont work with Entry items (must type Enter after typing in password). UserID is corten temporarily

---
+++ CompanionApp/Companion/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace Companion
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnEntryStart(object sender, EventArgs e)
+        {
+            incorrect.Text = "";
+        }
+
+        async void NavigateToID(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TaskPage());
+        }
+
+        void OnEnterClicked(object sender, EventArgs e)
+        {
+
+            // User ID check
+            // TODO: Actually implement some checks for proper UserID (ie not empty)
+            if (!string.IsNullOrEmpty(userIDEntry.Text) && userIDEntry.Text.Equals("corten"))
+            {
+                App.UserID = userIDEntry.Text;
+
+                // Password Verification
+                if (passwordEntry.Text.Equals(App.Password))
+                {
+                    NavigateToID(sender, e);
+                }
+                else
+                {
+                    incorrect.Text = "Password Incorrect! Please try again.";
+                }
+            }
+            else
+            {
+                incorrect.Text = "User ID Incorrect! Please try again.";
+            }
+        }
+    }
+}
---
+++ CompanionApp/Companion/TaskPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Companion
+{
+    public partial class TaskPage : ContentPage
+    {
+        public TaskPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnSpeechClicked(object sender, EventArgs e)
+        {
+            // TODO: Navigate to a RecordingPage
+        }
+
+        async void OnNextClicked(object sender, EventArgs e)
+        {
+            // This is just a placeholder for the next task button we add
+            await Navigation.PopAsync();
+        }
+    }
+}
